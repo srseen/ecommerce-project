4caa9d01fb27ea04e66027e84ad19c17e8bd001b
srseen <seereen.2540@gmail.com>
2025-07-03 17:01:22 +0700

    feat: add routing for ProductDetail and NotFound pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Index from "./pages/Index";
+import ProductDetail from "./pages/ProductDetail";
+import NotFound from "./pages/NotFound";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
